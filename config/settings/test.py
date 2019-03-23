b4b3e319750d9f198204007c4d4a04dd06ed6bdc
Haneul Lee <lovesky4294@gmail.com>
2019-03-23 16:56:40 +0900

    cookiecutter + cleanUp

--- config/settings/test.py
+++ config/settings/test.py
@@ -10,7 +10,10 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = False
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = env("DJANGO_SECRET_KEY", default="tKEGw3iV6HT5XAxqwjLrRfSVPuJGcypNeyzGujmrduCmUZaHvfCnYu1QFw7Q1iv4")
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="hjS1yO08dvRVIP7b9GdkPaavcm3cVjLoWd2ZhFGKNBAEoAqiFwqeGsXJCCEzEOxK",
+)
 # https://docs.djangoproject.com/en/dev/ref/settings/#test-runner
 TEST_RUNNER = "django.test.runner.DiscoverRunner"
 
@@ -19,7 +22,8 @@ TEST_RUNNER = "django.test.runner.DiscoverRunner"
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.locmem.LocMemCache", "LOCATION": ""
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "",
     }
 }
 
