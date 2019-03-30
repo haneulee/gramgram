1f7cbad3f83040e3742fb6e05d060d66a562eae0
Haneul Lee <lovesky4294@gmail.com>
2019-03-30 14:58:21 +0900

    fixed error

--- gramgram/images/views.py
+++ gramgram/images/views.py
@@ -3,18 +3,6 @@ from rest_framework.response import Response
 from . import models, serializers
 from django.shortcuts import get_object_or_404
 
-
-# class ListAllImages(APIView):
-
-    # def get(self, request, format=None):
-
-    #     all_images = models.Image.objects.all()
-
-    #     serializer = serializers.ImageSerializer(all_images, many=True)
-
-    #     return Response(data=serializer.data)
-
-
 class Feed(APIView):
 
     def get(self, request, format=None):
@@ -27,22 +15,21 @@ class Feed(APIView):
 
         for following_user in following_users:
 
-            print(following_users.images.all()[:2])
-
-            user_images = following_users.images.all()[:2]
+            user_images = following_user.images.all()[:2]
 
             for image in user_images:
 
                 image_list.append(image)
 
-        sorted_list = sorted(image_list, key=lambda image: image.create_at, reverse=True)
+        sorted_list = sorted(
+            image_list, key=lambda image: image.created_at, reverse=True)
 
         serializer = serializers.ImageSerializer(sorted_list, many=True)
 
         return Response(serializer.data)
 
     # def get_key(image):
-    #     return image.create_at
+    #     return image.created_at
 
 # class ListAllComments(APIView):
 
