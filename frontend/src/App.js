a6c05b032ca15e96dcae9b211e82de0f6be0689f
Haneul Lee <lovesky4294@gmail.com>
2019-05-02 01:40:00 +0900

    set up redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,24 +4,7 @@ import "./App.scss";
 fetch("/notifications/");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App__header">
-        <img className="App__logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App__link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
