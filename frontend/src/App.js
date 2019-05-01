c6f254f38a8dcce7411476602124f3aa66994e22
Haneul Lee <lovesky4294@gmail.com>
2019-05-02 01:44:55 +0900

    change NODE_PATH

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
-import "./App.scss";
+import React, { Component } from "react";
+import "App.scss";
 
-fetch("/notifications/");
-
-function App() {
-  return <div className="App" />;
+class App extends Component {
+  render() {
+    return <div className="App" />;
+  }
 }
 
 export default App;
