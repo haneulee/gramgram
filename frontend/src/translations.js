5d0ffb22e1d39a8e38b660413d46e1ba00aa547a
Haneul Lee <lovesky4294@gmail.com>
2019-05-11 17:07:50 +0900

    translate lan with context

--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,5 +1,18 @@
 export const translations = {
   es: {
     "Login in": "Iniciar Ses"
+  },
+  kr: {
+    "Login in": "로그인",
+    "About Us": "소개",
+    Support: "지원",
+    Blog: "블로그",
+    Press: "기사",
+    API: "API",
+    Jobs: "채용",
+    Privacy: "개인",
+    Terms: "약관",
+    Directory: "연락",
+    Language: "언어"
   }
 };
