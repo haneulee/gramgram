4e3a81ed8d2db8b97b8432aad176e7bf89a179b1
Haneul Lee <lovesky4294@gmail.com>
2019-06-08 19:30:31 +0900

    add like, dislike photo action

--- frontend/src/components/PhotoActions/presenter.js
+++ frontend/src/components/PhotoActions/presenter.js
@@ -1,22 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { FiHeart, FiMessageCircle } from "react-icons/fi";
+import { FaHeart, FaRegHeart, FaRegComment } from "react-icons/fa";
 import styles from "./styles.module.scss";
 
 const PhotoActions = (props, context) => {
   return (
     <div className={styles.actions}>
       <div className={styles.icons}>
-        <span className={styles.icon}>
-          <FiHeart fontSize="28px" />
+        <span className={styles.icon} onClick={props.handleHeartClick}>
+          {props.isLiked ? (
+            <FaHeart fontSize="28px" color="#EB4B59" />
+          ) : (
+            <FaRegHeart fontSize="28px" color="black" />
+          )}
         </span>
         <span className={styles.icon}>
-          <FiMessageCircle fontSize="28px" />
+          <FaRegComment fontSize="28px" />
         </span>
       </div>
       <span className={styles.likes}>
         {props.number}{" "}
-        {props.number === 1 ? context.t("like") : context.t("likes")}
+        {props.number > 1 ? context.t("likes") : context.t("like")}
       </span>
     </div>
   );
@@ -27,7 +31,10 @@ PhotoActions.contextTypes = {
 };
 
 PhotoActions.propTypes = {
-  number: PropTypes.number.isRequired
+  number: PropTypes.number.isRequired,
+  isLiked: PropTypes.bool.isRequired,
+  photoId: PropTypes.number.isRequired,
+  handleHeartClick: PropTypes.func.isRequired
 };
 
 export default PhotoActions;
--- frontend/src/redux/modules/photos.js
+++ frontend/src/redux/modules/photos.js
@@ -5,6 +5,8 @@ import { actionCreators as userActions } from "redux/modules/users";
 // actions
 
 const SET_FEED = "SET_FEED";
+const LIKE_PHOTO = "LIKE_PHOTO";
+const DISLIKE_PHOTO = "DISLIKE_PHOTO";
 
 // action creators
 
@@ -12,6 +14,20 @@ function setFeed(feed) {
   return {
     type: SET_FEED,
     feed
+  };
+}
+
+function doLikePhoto(photoId) {
+  return {
+    type: LIKE_PHOTO,
+    photoId
+  };
+}
+
+function doDislikePhoto(photoId) {
+  return {
+    type: DISLIKE_PHOTO,
+    photoId
   };
 }
 
@@ -42,6 +58,48 @@ function getFeed() {
   };
 }
 
+function likePhoto(photoId) {
+  return function(dispatch, getState) {
+    dispatch(doLikePhoto(photoId));
+    const {
+      users: { token }
+    } = getState();
+    fetch(`/images/${photoId}/likes/`, {
+      method: "POST",
+      headers: {
+        Authorization: `JWT ${token}`
+      }
+    }).then(response => {
+      if (response.status === 401) {
+        dispatch(userActions.logout());
+      } else if (!response.ok) {
+        dispatch(doDislikePhoto(photoId));
+      }
+    });
+  };
+}
+
+function dislikePhoto(photoId) {
+  return function(dispatch, getState) {
+    dispatch(doDislikePhoto(photoId));
+    const {
+      users: { token }
+    } = getState();
+    fetch(`/images/${photoId}/unlikes/`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `JWT ${token}`
+      }
+    }).then(response => {
+      if (response.status === 401) {
+        dispatch(userActions.logout());
+      } else if (!response.ok) {
+        dispatch(doLikePhoto(photoId));
+      }
+    });
+  };
+}
+
 // initial state
 
 const initialState = {};
@@ -52,6 +110,10 @@ function reducer(state = initialState, action) {
   switch (action.type) {
     case SET_FEED:
       return applySetFeed(state, action);
+    case LIKE_PHOTO:
+      return applyLikePhoto(state, action);
+    case DISLIKE_PHOTO:
+      return applyDislikePhoto(state, action);
     default:
       return state;
   }
@@ -68,11 +130,43 @@ function applySetFeed(state, action) {
   };
 }
 
+function applyLikePhoto(state, action) {
+  const { photoId } = action;
+  const { feed } = state;
+
+  const updatedFeed = feed.map(photo => {
+    if (photo.id === photoId) {
+      return { ...photo, is_liked: true, like_count: photo.like_count + 1 };
+    }
+
+    return photo;
+  });
+
+  return { ...state, feed: updatedFeed };
+}
+
+function applyDislikePhoto(state, action) {
+  const { photoId } = action;
+  const { feed } = state;
+
+  const updatedFeed = feed.map(photo => {
+    if (photo.id === photoId) {
+      return { ...photo, is_liked: false, like_count: photo.like_count - 1 };
+    }
+
+    return photo;
+  });
+
+  return { ...state, feed: updatedFeed };
+}
+
 // exports
 
 const actionCreators = {
   getFeed,
-  setFeed
+  setFeed,
+  likePhoto,
+  dislikePhoto
 };
 
 export { actionCreators };
