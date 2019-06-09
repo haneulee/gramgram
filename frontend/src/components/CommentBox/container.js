151a500db1124a5464c0ff03c8733d800cb6c1f4
Haneul Lee <lovesky4294@gmail.com>
2019-06-09 12:32:37 +0900

    add comment to redux store

--- frontend/src/components/CommentBox/container.js
+++ frontend/src/components/CommentBox/container.js
@@ -39,6 +39,9 @@ class Container extends Component {
     if (key === "Enter") {
       event.preventDefault();
       submitComment(comment);
+      this.setState({
+        comment: ""
+      });
     }
   };
 }
--- frontend/src/redux/modules/photos.js
+++ frontend/src/redux/modules/photos.js
@@ -7,6 +7,7 @@ import { actionCreators as userActions } from "redux/modules/users";
 const SET_FEED = "SET_FEED";
 const LIKE_PHOTO = "LIKE_PHOTO";
 const DISLIKE_PHOTO = "DISLIKE_PHOTO";
+const ADD_COMMENT = "ADD_COMMENT";
 
 // action creators
 
@@ -28,6 +29,14 @@ function doDislikePhoto(photoId) {
   return {
     type: DISLIKE_PHOTO,
     photoId
+  };
+}
+
+function addComment(photoId, comment) {
+  return {
+    type: ADD_COMMENT,
+    photoId,
+    comment
   };
 }
 
@@ -114,11 +123,19 @@ function commentPhoto(photoId, message) {
       body: JSON.stringify({
         message
       })
-    }).then(response => {
-      if (response.status === 401) {
-        dispatch(userActions.logout());
-      }
-    });
+    })
+      .then(response => {
+        if (response.status === 401) {
+          dispatch(userActions.logout());
+        }
+
+        return response.json();
+      })
+      .then(json => {
+        if (json.message) {
+          dispatch(addComment(photoId, json));
+        }
+      });
   };
 }
 
@@ -136,6 +153,8 @@ function reducer(state = initialState, action) {
       return applyLikePhoto(state, action);
     case DISLIKE_PHOTO:
       return applyDislikePhoto(state, action);
+    case ADD_COMMENT:
+      return applyAddComment(state, action);
     default:
       return state;
   }
@@ -174,6 +193,21 @@ function applyDislikePhoto(state, action) {
   const updatedFeed = feed.map(photo => {
     if (photo.id === photoId) {
       return { ...photo, is_liked: false, like_count: photo.like_count - 1 };
+    }
+
+    return photo;
+  });
+
+  return { ...state, feed: updatedFeed };
+}
+
+function applyAddComment(state, action) {
+  const { photoId, comment } = action;
+  const { feed } = state;
+
+  const updatedFeed = feed.map(photo => {
+    if (photo.id === photoId) {
+      return { ...photo, comments: [...photo.comments, comment] };
     }
 
     return photo;
